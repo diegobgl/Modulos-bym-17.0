9310e70ce7f239d1f60c1d730ec9e9cb3f78d87a
Diego Gajardo <deskodoo@a2group.cl>
2024-05-26 23:26:04 -0400

    first commit

---
+++ l10n_cl_counties/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Chile Localization Regions, Cities and Counties",
+    "version": "17.0",
+    "author": "Blanco Martín & Asociados",
+    'license': "LGPL-3",
+    "website": "http://blancomartin.cl",
+    "category": "Localization/Geopolitical Distribution",
+    "depends": [
+        "l10n_cl",
+        "base_address_extended",
+    ],
+    "data": [
+        "data/res.country.state.csv",
+        "data/res.city.csv",
+        "views/res_partner_view.xml",
+        "views/res_company_view.xml",
+    ],
+    "active": False,
+    "installable": True,
+}
